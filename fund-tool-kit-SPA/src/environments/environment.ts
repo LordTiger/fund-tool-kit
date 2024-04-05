5cecb0bdebbdc4893b2df5d8d299aac58ee7d51d
Brandon de Bruyn <31498965+LordTiger@users.noreply.github.com>
2024-04-05 10:22:09 +0200

    add: supabase service

---
+++ fund-tool-kit-SPA/src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: true,
+    supabaseUrl: 'https://jxpiuxoutuxqmgrzyffj.supabase.co',
+    supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imp4cGl1eG91dHV4cW1ncnp5ZmZqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTIzMDQzMjcsImV4cCI6MjAyNzg4MDMyN30.xptpkMafPrDXWCiMfdjbGZDMTi-ZjN_OtKQFyI6Hwbk',
+  }
